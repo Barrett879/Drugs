0fad700b193b3936febc28293ea9a5b2be67bda0
Barrett879 <77691102+Barrett879@users.noreply.github.com>
2021-04-25 14:45:23 -0700

    Update python.py

--- python.py
+++ python.py
@@ -9,30 +9,8 @@ def home():
     return render_template('home.html', options=get_state_options())
 
 @app.route('/funFact')
-def render_fun_fact():
-    state_chosen = request.args['states']
-    return render_template('home.html', options=get_state_options(), funFact=fun_fact_by_state(state_chosen))
-  
-def get_state_options():
-    listOfStates = []
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    for county in counties:
-        if not(county["State"] in listOfStates): 
-            listOfStates.append(county["State"])
-    options = ""
-    for state in listOfStates:
-        options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
-    return options
 
-def fun_fact_by_state(state):
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    first_county = "ZZZZZZZZ"
-    for county in counties:
-        if county["County"] < first_county and county["State"] == state:
-            first_county = county["County"]
-    return first_county
+  
   
 if __name__=='__main__':
     app.run(debug=True)
